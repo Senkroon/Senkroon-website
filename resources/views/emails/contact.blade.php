a024bcceb93a67607b01e622d1d616871faa49bb
Celalettin Elbir <celallelbir@hotmail.com>
2026-02-18 18:28:45 +0300

    fix: add missing fields to contact form email template

--- resources/views/emails/contact.blade.php
+++ resources/views/emails/contact.blade.php
@@ -7,7 +7,12 @@
     <h1>İletişim Formu Mesajı</h1>
     <p><strong>Ad Soyad:</strong> {{ $data['name'] }}</p>
     <p><strong>E-posta:</strong> {{ $data['email'] }}</p>
+
+    <p><strong>Telefon:</strong> {{ $data['phone'] }}</p>
+    <p><strong>Konu:</strong> {{ $data['subject'] }}</p>
+    <p><strong>Şirket:</strong> {{ $data['company'] }}</p>
     <p><strong>Mesaj:</strong></p>
+
     <p>{{ $data['message'] }}</p>
 </body>
 </html>
